88624d94e3e2d631ffd01252b671a8b8e8a0fa53
FieryRMS <raiyanmohdshahrms@gmail.com>
2021-07-24 01:37:11 +0600

    asd

--- src/components/Timer2.vue
+++ src/components/Timer2.vue
@@ -14,44 +14,33 @@
 
 
 <script>
-(function () {
-  const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
-
-  let birthday = "Sep 30, 2021 00:00:00",
-    countDown = new Date(birthday).getTime(),
-    x = setInterval(function () {
-      let now = new Date().getTime(),
-        distance = countDown - now;
-
-      (document.getElementById("days").innerText = Math.floor(distance / day)),
-        (document.getElementById("hours").innerText = Math.floor(
-          (distance % day) / hour
-        )),
-        (document.getElementById("minutes").innerText = Math.floor(
-          (distance % hour) / minute
-        )),
-        (document.getElementById("seconds").innerText = Math.floor(
-          (distance % minute) / second
-        ));
-
-      //do something later when date is reached
-      if (distance < 0) {
-        let headline = document.getElementById("headline"),
-          countdown = document.getElementById("countdown"),
-          content = document.getElementById("content");
-
-        headline.innerText = "It's my birthday!";
-        countdown.style.display = "none";
-        content.style.display = "block";
-
-        clearInterval(x);
-      }
-      //seconds
-    }, 0);
-})();
+export default {
+  mounted() {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60;
+    let countDown = Date.now() + 1000 * 10 * 1,
+      x = setInterval(function () {
+        let distance = countDown - Date.now();
+        if (distance <= 0) {
+          clearInterval(x);
+          document.getElementById("days").innerText = 0;
+          document.getElementById("hours").innerText = 0;
+          document.getElementById("minutes").innerText = 0;
+          document.getElementById("seconds").innerText = 0;
+          return;
+        }
+        document.getElementById("days").innerText = Math.floor(distance / day);
+        document.getElementById("hours").innerText = 
+          Math.floor((distance % day) / hour);
+        document.getElementById("minutes").innerText =
+          Math.floor((distance % hour) / minute);
+        document.getElementById("seconds").innerText =
+          Math.floor((distance % minute) / second);
+        //seconds
+      }, 0);
+  },
+};
 </script>
 
 <style scoped>
--- src/components/Timer3.vue
+++ src/components/Timer3.vue
@@ -1,31 +1,34 @@
 <template>
-    <div class="timer-text">
-      <span id="minutes" class="pr-2"></span>
-      <span id="seconds"></span>
-    </div>
+  <div class="timer-text">
+    <span id="minutes" class="pr-2"></span>
+    <span id="seconds"></span>
+  </div>
 </template>
 
 
 <script>
-const second = 1000,
-  minute = second * 60,
-  hour = minute * 60;
-let birthday = "Sep 30, 2021 00:00:00",
-  countDown = new Date(birthday).getTime(),
-  x = setInterval(function () {
-    let now = new Date().getTime(),
-      distance = countDown - now;
-    (document.getElementById("minutes").innerText =
-      Math.floor((distance % hour) / minute) + "min"),
-      (document.getElementById("seconds").innerText =
-        Math.floor((distance % minute) / second) + "sec");
-    //do something later when date is reached
-    if (distance < 0) {
-      clearInterval(x);
-    }
-    //seconds
-  }, 0);
-console.log(countDown);
+export default {
+  mounted() {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60;
+    let countDown = Date.now() + 1000 * 10 * 1,
+      x = setInterval(function () {
+        let distance = countDown - Date.now();
+        if (distance <= 0) {
+          clearInterval(x);
+          document.getElementById("minutes").innerText = "Contest";
+          document.getElementById("seconds").innerText = "ended";
+          return;
+        }
+        document.getElementById("minutes").innerText =
+          Math.floor((distance % hour) / minute) + "min";
+        document.getElementById("seconds").innerText =
+          Math.floor((distance % minute) / second) + "sec";
+        //seconds
+      }, 0);
+  },
+};
 </script>
 
 <style scoped>
@@ -38,25 +41,23 @@ ul {
   padding: 0;
 }
 
-
 li {
   display: inline-block;
   font-size: 1.5em;
   list-style-type: none;
   /* padding: 1em; */
-  text-transform: lowercase;
 }
 
 li span {
   display: block;
   font-size: 2rem;
 }
-.timer-text{
+.timer-text {
   color: rgb(255, 255, 255);
   text-shadow: 0 0 20px rgba(10, 175, 230, 1), 0 0 20px rgba(255, 255, 255, 0);
   padding: 0px;
-  display:inline-block;
-  font-size:30px;
+  display: inline-block;
+  font-size: 30px;
   text-transform: lowercase;
 }
 </style>
